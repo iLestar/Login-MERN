d7a7d26239d2117d694ff8b9544ec2d54e9eee08
iLestar <juanlestar0408@gmail.com>
2023-07-29 11:37:09 -0500

    Token middleware validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -41,3 +41,21 @@ export const login = async (req, res) => {
     }
      
 }
+
+export const logout = (req, res) => {
+    res.cookie('token', '', {
+        expires: new Date(0),
+    })
+    return res.sendStatus(200)
+}
+
+export const profile = async (req, res) => {
+    const userFound = await User.findById(req.user.id)
+    if(!userFound) return res.status(400).json({message: 'User not found'})
+    res.status(201).json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email
+    })
+}
+
