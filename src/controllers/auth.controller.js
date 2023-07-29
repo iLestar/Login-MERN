18299f878b013b4e5b73552b8f615764d011d1ff
iLestar <juanlestar0408@gmail.com>
2023-07-29 10:33:03 -0500

    Register functional and password hashed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,23 @@
 import User from '../models/user.models.js'
+import bcrypt from 'bcryptjs'
 
-export const register = (req, res) => res.send('register')
+export const register = async (req, res) => {
+    const { username, email, password } = req.body
+    try {
+        const passwordHash = await bcrypt.hash(password, 10)
+        const newUser = new User({username, email, password: passwordHash})
+        const userSaved = await newUser.save()
+        res.status(201).json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email
+        })
+    } catch (error) {
+        res.status(500).json({messahe: error.message})
+    }
+     
+}
+
+
+
 export const login = (req, res) => res.send('login')
