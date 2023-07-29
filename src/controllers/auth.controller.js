1ef14e6f6ed17eb0dc5e36d8c34182b4fc9d851b
iLestar <juanlestar0408@gmail.com>
2023-07-29 11:00:21 -0500

    Functional login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,6 +21,23 @@ export const register = async (req, res) => {
      
 }
 
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const userFound = await User.findOne({email})
+        if(!userFound) return res.status(400).json({message: 'User not found'})
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        if(!isMatch) return res.status(400).json({message: 'Error in credentials'})
 
-
-export const login = (req, res) => res.send('login')
+        const token = await createTokenAccess({ id: userFound._id })
+        res.cookie('token', token)
+        res.status(201).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        })
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+     
+}
