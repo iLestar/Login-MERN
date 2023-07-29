ee3b22272933d5b01e6e832af90b7987fa230849
iLestar <juanlestar0408@gmail.com>
2023-07-29 10:48:26 -0500

    Functional Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.models.js'
 import bcrypt from 'bcryptjs'
+import { createTokenAccess } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body
@@ -7,6 +8,8 @@ export const register = async (req, res) => {
         const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({username, email, password: passwordHash})
         const userSaved = await newUser.save()
+        const token = await createTokenAccess({ id: userSaved._id })
+        res.cookie('token', token)
         res.status(201).json({
             id: userSaved._id,
             username: userSaved.username,
